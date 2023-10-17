dafa6e8215aaea8ed71baba6c863839df75ca7f6
Kriukov Roman <romank.official@gmail.com>
2023-10-17 17:25:41 +0300

    Deleted spring security. For the creating sessions have added class HttpSession

--- src/main/java/com/example/connectdb/controller/LoginController.java
+++ src/main/java/com/example/connectdb/controller/LoginController.java
@@ -1,75 +1,71 @@
 package com.example.connectdb.controller;
 
 import com.example.connectdb.service.ConnectionDBService;
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 import java.sql.Connection;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
 @Controller
 public class LoginController {
 
     @Autowired
-    ConnectionDBService connectionDB;
+    private ConnectionDBService connectionDB;
 
-    Connection connection;
+    @Resource
+    HttpSession session;
 
     @GetMapping("/login1")
-    public String showLoginPage(){
-
-        System.out.println("smth write!!!");
+    public String showLoginPage() {
+        System.out.println("GET!!!");
         return "login";
     }
 
     @PostMapping("/login")
-    public String authenticate(@RequestParam String username, @RequestParam String password){
-        connection = connectionDB.connectToDB(username, password);
-        ResultSet resultSet = null;
-        try {
-            resultSet = connectionDB.query("select name = system_user", connection);
-        }catch (Exception e){
-            System.out.println("Не вдалося отримати дані!");
+    public String authenticate(@RequestParam String username, @RequestParam String password) {
+        System.out.println("POST!!!");
+        Connection conn = connectionDB.connectToDB(username, password);
+
+        if (conn != null) {
+            session.setAttribute("username", conn);
+            return "home";
+        } else {
+            return "login";
         }
-        String str = "";
-        try {
-            if (resultSet != null) {
-                while (resultSet.next()) {
-                    str = str + ", " + resultSet.getString("name");
-                }
-            }
-        }catch(Exception e){
-            System.out.println(e.getMessage());
-        }
-        System.out.println("login: " + username + " password: " + password + " SystemUser: " + str);
-        return "home";
     }
 
     @GetMapping("/other")
-    public String otherPoint(){
+    public String otherPoint() {
+        Connection conn = (Connection) session.getAttribute("username");
 
-        ResultSet resultSet = null;
-        try {
-            resultSet = connectionDB.query("select name = system_user", connection);
-        }catch (Exception e){
-            System.out.println("Не вдалося отримати дані!");
-        }
-        String str = "";
-        try {
-            if (resultSet != null) {
-                while (resultSet.next()) {
-                    str = str + ", " + resultSet.getString("name");
-                }
+        if (conn != null) {
+            ResultSet resultSet = null;
+            try {
+                resultSet = connectionDB.query("select * from userRoles(null)", conn);
+            } catch (Exception e) {
+                System.out.println("Failed to retrieve data!");
             }
-        }catch(Exception e){
-            System.out.println(e.getMessage());
+            String str = "";
+            try {
+                if (resultSet != null) {
+                    while (resultSet.next()) {
+                        str = str + resultSet.getString("role") + " ";
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            System.out.println("Roles: " + str);
+            return "home";
+        } else {
+            return "login";
         }
-        System.out.println("SystemUser: " + str);
-        return "home";
     }
-
 }
